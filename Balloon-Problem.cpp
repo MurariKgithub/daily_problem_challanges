0315225988fab41c94a386a88fdcd01f59e5ceec
MurariKgithub <115924750+MurariKgithub@users.noreply.github.com>
2023-02-10 15:33:42 +0530

    Update Balloon-Problem.cpp
    Bob is very fond of balloons. Once he visited an amusement park with his mother. The mother told Bob that she would buy him a balloon only if he answer her problem right. She gave Bob a string S [contains only lowercase characters] and asked him to use the characters of string to form as many instances of the word "balloon" as possible. Return the maximum number of instances that can be formed.
    Help Bob to solve the problem.
    Note: You can use each character in the string at most once.
    
    Input:
    S: nlaebolko
    Output: 1
    Explanation:
    Here, the number of occurence of 'b' = 1
    of occurence of 'a' = 1
    of occurence of 'l' = 2
    of occurence of 'o' = 2
    of occurence of 'n' = 1
    So, we can form 1 "balloon" using the letters.
    Date: 10/02/2023 GFG easy

--- Balloon-Problem.cpp
+++ Balloon-Problem.cpp
@@ -3,9 +3,11 @@ public:
     int maxInstance(string s){
         map<char,int> frq,actual;
         string ans = "balloon";
+        // step1 : actual map crate with no of ballon letter requires 
         for(auto it:ans) actual[it]++;
+        // step 2; create frq map 
         for(auto it:s) frq[it]++;
-        
+       
         int res =INT_MAX;
         for(auto it:actual){
             res =min(res,frq[it.first]/actual[it.first]);
