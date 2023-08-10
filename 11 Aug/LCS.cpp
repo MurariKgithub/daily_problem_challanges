db5acc53d439407e09524e249914675c7278b2f6
MurariKgithub <115924750+MurariKgithub@users.noreply.github.com>
2023-08-11 00:46:21 +0530

    Create LCS.cpp
    Problem is LCS in string 
    Standered problem based on DP
    T.C = O(n*n)

---
+++ 11 Aug/LCS.cpp
@@ -0,0 +1,15 @@
+vector<vector<int>> dp;
+    //Function to find the length of longest common subsequence in two strings.
+    
+    int lcs(int n, int m, string s1, string s2)
+    {
+        dp.resize(n+1,vector<int>(m+1,0));
+        for(int i=n-1 ;i>=0 ;i--){
+            for(int j=m-1 ; j>=0; j--){
+                
+                if(s1[i]==s2[j])  dp[i][j] = 1+dp[i+1][j+1];
+                else dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+            }            
+        }
+        return dp[0][0];
+    }
